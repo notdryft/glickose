f1d5b7f0aacf33b2621da93bc135171f66336c53
notdryft <gui.corre@gmail.com>
2017-04-04 08:02:33 +0200

    Fix indentation

--- dev-server.js
+++ dev-server.js
@@ -5,12 +5,12 @@ const config = require('./webpack.config.development.js');
 
 const compiler = webpack(config);
 const server = new WebpackDevServer(compiler, {
-    hot: true,
-    filename: config.output.filename,
-    publicPath: config.output.publicPath,
-    stats: {
-        colors: true
-    }
+  hot: true,
+  filename: config.output.filename,
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true
+  }
 });
 
 server.listen(3000, 'localhost', () => {
